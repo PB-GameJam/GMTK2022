4d3b3bff191866af1ee61ce63558dc3d13e408b3
Felix Knight <felixmaeknight@gmail.com>
2022-07-16 17:30:27 -0700

    made the UI for powerups

---
+++ Assets/Scripts/PowerupData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupData : MonoBehaviour
+{
+    //will maybe make a scriptable obj for powerup data to keep it clearer? Probably not though
+}
