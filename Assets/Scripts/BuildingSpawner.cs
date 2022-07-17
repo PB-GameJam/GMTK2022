4cd52acf99cc13c6f53332fe6aa5912c5d4dc482
Mosensta <EricM.Moser@outlook.com>
2022-07-17 07:12:45 -0700

    Eric Scene Scenery

--- Assets/Scripts/BuildingSpawner.cs
+++ Assets/Scripts/BuildingSpawner.cs
@@ -8,14 +8,31 @@ using UnityEngine;
 public class BuildingSpawner : MonoBehaviour
 {
     public List<GameObject> BuildingPool;
+    public float RoadCheckRadius = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
         int spawn = 0;
         spawn = Random.Range(0, BuildingPool.Count);
-        Instantiate(BuildingPool[spawn],transform);
+        FindRoad();
+        Instantiate(BuildingPool[spawn],transform.position,Quaternion.identity);
 
+    }
+
+    public void FindRoad()
+    {
+        Collider[] sensed = Physics.OverlapSphere(transform.position, RoadCheckRadius);
+        foreach (Collider item in sensed)
+        {
+            Debug.Log(item.name);
+            if (item.CompareTag("Road"))
+            {
+                
+                Vector3 rotationDir = (item.transform.position - transform.position);
+                transform.rotation = Quaternion.Euler(new Vector3(0, rotationDir.y, 0));
+            }
+        }
     }
 
     // Update is called once per frame
